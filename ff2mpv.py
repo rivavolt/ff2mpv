rivavolt/ff2mpv

commit 6b52219c2823a0e441ab87216218001d67b55759

fix(windows): fix process spawning (#36)
Fixes the Python client on Windows to ensure that MPV is spawned with the appropriate process flags.

Fixes #26.

ff2mpv.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 
-import sys
-import struct
 import json
-from subprocess import Popen, DEVNULL
+import platform
+import struct
+import sys
+from subprocess import Popen, CREATE_BREAKAWAY_FROM_JOB
 
 
 def main():
     message = get_message()
     url = message.get("url")
 
-    mpv_args = ["--no-terminal"]
+    args = ["mpv", "--no-terminal", "--", url]
 
-    args = ["mpv", *mpv_args, "--", url]
-    Popen(args, stdin=DEVNULL, stdout=DEVNULL, stderr=DEVNULL)
+    kwargs = {}
+    # https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Native_messaging#Closing_the_native_app
+    if platform.system() == "Windows":
+        kwargs["creationflags"] = CREATE_BREAKAWAY_FROM_JOB
+
+    Popen(args, **kwargs)
+
     # Need to respond something to avoid "Error: An unexpected error occurred"
     # in Browser Console.
     send_message("ok")

ff2mpv.js
@@ -1,8 +1,12 @@
+function onError(error) {
+    console.log(`${error}`);
+}
+
 function ff2mpv(url) {
     browser.tabs.executeScript({
         code: "video = document.getElementsByTagName('video');video[0].pause();"
     });
-    browser.runtime.sendNativeMessage("ff2mpv", { url: url });
+    browser.runtime.sendNativeMessage("ff2mpv", { url: url }).catch(onError);
 }
 
 browser.contextMenus.create({

commit 0f9ba414f61ab0ff12655d69c55b58d88afbf009

fix(pyclient): Avoid broken import on non-Windows
Fixes an import error on non-Windows introduced in #36.

See https://github.com/woodruffw/ff2mpv/pull/36#issuecomment-721438656.

ff2mpv.py
@@ -4,7 +4,7 @@ import json
 import platform
 import struct
 import sys
-from subprocess import Popen, CREATE_BREAKAWAY_FROM_JOB
+import subprocess
 
 
 def main():
@@ -16,9 +16,9 @@ def main():
     kwargs = {}
     # https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Native_messaging#Closing_the_native_app
     if platform.system() == "Windows":
-        kwargs["creationflags"] = CREATE_BREAKAWAY_FROM_JOB
+        kwargs["creationflags"] = subprocess.CREATE_BREAKAWAY_FROM_JOB
 
-    Popen(args, **kwargs)
+    subprocess.Popen(args, **kwargs)
 
     # Need to respond something to avoid "Error: An unexpected error occurred"
     # in Browser Console.